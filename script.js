14b0978778aae0523c9352dfd8c138c6e93b6bad
douglaschu <douglas.l.chu@gmail.com>
2020-05-11 18:56:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const randomDamage = () => {
+    return Math.floor(Math.random()* 10 + 1);
+}
+console.log(randomDamage());
+
+//to add an armor stat, replace above with:
+const randomDamageArmor = (armor) => {
+    let damage = Math.floor(Math.random()* 10 + 1);
+    
+    damage = damage - armor;
+    if(damage < 0){
+        damage = 0;
+    } 
+    return damage;
+}
+console.log(randomDamageArmor(2));
+
+
+const chooseOption = (opt1, opt2) => {
+    let randNum = Math.round(Math.random());
+    return (randNum%2 === 0) ? opt1 :  opt2;  
+}
+console.log(chooseOption('Heads','Tails'));
+
+const attackPlayer = function(health){
+    return health - randomDamage();
+}
+console.log(attackPlayer(100));
+
+const logHealth = (player, health) => {    
+    console.log(`${player} health: ${health}`);
+}
+logHealth('Tiger Mask', 100);
+
+const logDeath = (winner, loser) => {
+    console.log(`${winner} defeated ${loser}.`);
+}
+logDeath("Stone Cold", "The Rock");
+
+const isDead = (health) => {
+    return health <= 0 ? true : false;
+}
+console.log(isDead(-5));
+
+function fight(player1, player2, player1Health, player2Health){
+
+    console.log(`${player1} vs. ${player2}! FIGHT!!`);
+    
+    while (true) {
+        const attacker = chooseOption(player1, player2);
+        if (attacker === player1) {
+            player2Health = attackPlayer(player2Health);
+            logHealth(player2, player2Health);
+            if (isDead(player2Health)) {
+                logDeath(player1, player2);
+                break;
+            }
+        else {
+            player1Health = attackPlayer(player1Health);
+            logHealth(player1, player1Health);
+            if (isDead(player1Health)) {
+                logDeath(player2, player1);
+                break;
+            }
+        }
+        }
+    }
+}
+
+fight(`Sasha Banks`, `Io Shirai`, 100, 100);
